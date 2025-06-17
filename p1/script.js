f41f28dc04c64570b9bf407d933045c75f10541f
khushi11github <khushik110012@gmail.com>
2025-06-17 10:29:29 +0530

    commit

---
+++ p1/script.js
@@ -0,0 +1,36 @@
+const loginForm = ()=>{
+    const str = `<div>
+         <h3>Login Form </h3>
+         <input name ="email" placeholder="email"><br><br>
+         <input name ="password" type="password" placeholder="password"><br>
+         <p><button onClick='showHome()'>submit</button></p>
+
+        <p><button onClick='RegisterForm()'>Create Account</button></p>
+
+    `
+    root.innerHTML = str+ "</div>";
+}
+
+
+const RegisterForm=()=>{
+    const str=`
+    <div>
+    <h3>Register Form</h3>
+    <input name="name" placeholder="Name"><br><br>
+    <input name="email" placeholder="Email"><br><br>
+    <input name="password" type="password" placeholder="Password"><br><br>
+
+    <p><button onclick="loginForm()">Submit</button></p>
+    <p>Already a member? <button onclick="loginForm()">Login</button></p>
+    `
+    root.innerHTML=str+'</div>';
+}
+const showHome = ()=>{
+    const str = `<div>
+    <h1>Welcome to Home Page</h1>
+    <p><button onClick="loginForm()">Logout</button></p>
+    `
+    root.innerHTML = str+"</div>";
+
+
+}
