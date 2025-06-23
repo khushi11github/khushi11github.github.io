987f2de3885bc1586aad63fecad3c1363f106183
khushi11github <khushik110012@gmail.com>
2025-06-23 11:44:49 +0530

    commit

---
+++ javascript/file15.js
@@ -0,0 +1,22 @@
+//js objects
+
+// const student ={
+//     name:"omil",
+//     age:21,
+// };
+// console.log(JSON.stringify(student)); // {"name":"omil","age":21}
+
+
+// const student ='{
+//    "name":"omil",
+// "age":21,
+// }';
+// console.log(JSON.parse(student));
+
+
+
+const student = `{
+   "name": "omil",
+   "age": 21
+}`;
+console.log(JSON.parse(student));
