fb29829a38f876ef01051aecc97d7a97e13711c9
khushi11github <khushik110012@gmail.com>
2025-06-23 10:30:45 +0530

    commit

---
+++ javascript/file9.js
@@ -0,0 +1,27 @@
+const points=[2,5,3,2,1,8];
+points.forEach((value,index)=>{ 
+    console.log(value);
+});
+
+//Map
+// const newArr = points.map((value,index)=> {
+//     console.log(value);
+//     return value += 5;
+// }
+//  const newArr = points.map((value,index)=> ( value +=5));
+
+
+//FILTER
+//  const newArr = points.filter((value)=> ( value > 2));
+//  console.log(newArr);
+
+//find ->store the first value satisfying the condition
+//  const result = points.find((value)=> ( value  === 2));
+//  console.log(result);
+
+
+//reduce -> accumulate the values
+const result = points.reduce((sum,value)=>{
+    return sum + value;
+},0);
+console.log(result);
