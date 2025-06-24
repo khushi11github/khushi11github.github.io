e69e8ffc369f2ac702490cff1f5aa814c65e8767
khushi11github <khushik110012@gmail.com>
2025-06-24 10:52:23 +0530

    commit

--- javascript/file16.js
+++ javascript/file16.js
@@ -84,4 +84,12 @@ fetch("https://jsonplaceholder.typicode.com/users").then((res)=>res.json()).then
 
 
 
+const fetchData = async()=>{
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+    data.map((value)=>{
+        console.log(` ${value.name} `);
+    });
+}
 
+fetchData();
