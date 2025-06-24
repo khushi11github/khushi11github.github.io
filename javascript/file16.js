f16e0337c5256b6e12836d5c2b326ca0cb561525
khushi11github <khushik110012@gmail.com>
2025-06-24 10:37:00 +0530

    commit

---
+++ javascript/file16.js
@@ -0,0 +1,87 @@
+// //promise
+
+
+// const f1 =()=>{
+//     // setTimeout(() => {
+//     //      return 5;
+//     // }, 1000);
+
+   
+// }
+
+// const f2 =(x)=>{
+//     console.log(x+6);
+// };
+
+// const n= f1();
+// f2(n);
+
+
+//promise
+
+
+// const f1 =()=>{
+ 
+
+//     return new Promise((resolve,reject)=>{
+//         resolve(5);
+//     })
+   
+// }
+
+// const f2 =(x)=>{
+//     console.log(x+6);
+// };
+// f1().then((n) => f2(n));
+
+
+
+// const f1 =()=>{
+ 
+
+//     return new Promise((resolve,reject)=>{
+//         reject("Something went wrong");
+//     })
+   
+// }
+
+// const f2 =(x)=>{
+//     console.log(x+6);
+// };
+// f1().then((n) => f2(n)).catch((err) => console.log(err));
+
+
+// const f1 =(n)=>{
+ 
+
+//     return new Promise((resolve,reject)=>{
+//         if(n<0){
+//             reject("Number should be positive");
+//         }
+//         else resolve(n);
+
+
+//     })
+   
+// }
+
+// const f2 =(x)=>{
+   
+//     console.log(x+6);
+// };
+// f1(1).then((n) => f2(n)).catch((err) => console.log(err));
+
+//fetch return promise
+
+fetch("https://jsonplaceholder.typicode.com/users").then((res)=>res.json()).then((data)=>{
+    console.log(data);
+    data.map((value)=>{
+        console.log(`${value.id} - ${value.name} - ${value.email}`);
+    });
+
+    
+}).catch((err)=>console.log(err));
+
+
+
+
--- javascript/tempCodeRunnerFile.js
+++ javascript/tempCodeRunnerFile.js
@@ -1,3 +1,1 @@
-
-// const result2 = subtract(5,3);
-// console.log(result2);
+- ${value.email}
